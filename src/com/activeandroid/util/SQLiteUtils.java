b615cffd403781b42ddb82c8909a4e7dfe1710cf
Wendly.Li <wendly777@gmail.com>
2013-12-13 11:43:15 +0800

    Support create multi-group index

--- src/com/activeandroid/util/SQLiteUtils.java
+++ src/com/activeandroid/util/SQLiteUtils.java
@@ -153,8 +153,11 @@ public final class SQLiteUtils {
 			sIndexGroupMap.put(name, list);
 		}
 
-		String group = column.indexGroup();
-		if (!group.isEmpty()) {
+		String[] groups = column.indexGroups();
+		for (String group : groups) {
+			if (group.isEmpty())
+				continue;
+
 			List<String> list = sIndexGroupMap.get(group);
 			if (list == null) {
 				list = new ArrayList<String>();
